142fecb343115b8c2a613d9dc21b81c7e622414a
KAmron <koraboevkamron@gmail.com>
2023-03-05 02:20:54 +0500

    save

---
+++ main.js
@@ -0,0 +1,6 @@
+var number1 = +prompt('Введите первое число');
+var number2 = +prompt('Введите второе число');
+var number3 = +prompt('Введите третье число');
+var res = ((number1 + number2 + number3) * 1/3);
+console.log(res);
+alert('Ответ = ' + res)
